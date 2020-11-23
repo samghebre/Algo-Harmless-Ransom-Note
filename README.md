3c318dca172c297e818e98d964d06f8367ad4d6e
Samuel Ghebremedhin <samuelka2001@yahoo.com>
2020-11-22 19:48:11 -0500

    Completed Harmless Ransom note 11/22

--- README.md
+++ README.md
@@ -23,3 +23,16 @@ magazine_text= "puerto rico is a great place you must hike far from "
 ```
 
 The harmless ransom note will return ture because the word "secret" is used once and it's found once in the magazine text.
+
+
+Ransom Note Algorithm
+1 - Split Magazine String text to get an Array
+2 - Instantiate HashMap for Magazine (What word, How many times it appears)
+3 - Loop through Magazine Array
+4 - If HashMap contains KeyWord - get KeyValue Pair and Increment Integer count by 1
+5 - Else put element in the Hashmap and set Pair to 1
+6 - Split Ransom Note String text to get an Array
+7 - Loop through Ransom Note Array
+8 - If HashMap contains element - decrement Integer count by 1
+8a- If Integer count is 0, return false
+9 - Else If HashMap does not contain element - return false
--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,44 @@
 package hbcu.stay.ready.algor;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
 public class Solution {
 
-    public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+    public Boolean harmlessRansomNote(String message, String magazineText) {
+
+        String[] messageArray = message.split(" ");
+        String[] magazineTextArray = magazineText.split(" ");
+        HashMap<String, Integer> magazineTextMap = new HashMap<>();
+        HashMap<String, Integer> messageMap = new HashMap<>();
+
+        for (String word : magazineTextArray) {
+            if (magazineTextMap.containsKey(word)) {
+                magazineTextMap.put(word, magazineTextMap.get(word) + 1);
+            } else {
+                magazineTextMap.put(word, 1);
+            }
+        }
+        for (String word : messageArray) {
+            if (messageMap.containsKey(word)) {
+                messageMap.put(word, messageMap.get(word) + 1);
+            } else {
+                messageMap.put(word, 1);
+            }
+        }
+        for (Map.Entry<String, Integer> finalMessage : messageMap.entrySet()) {
+            String key = finalMessage.getKey();
+            int value = finalMessage.getValue();
+            if (value > magazineTextMap.get(key) || !magazineTextMap.containsKey(key)) {
+                return false;
+            }
+
+        }
+        return true;
     }
+
 }
